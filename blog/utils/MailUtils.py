939ee6bcf9de1cd1aa57989c305339d7c1d2f756
rholdberh <roman.goldberg@gmail.com>
2018-10-29 23:43:41 +0100

    bla

--- blog/utils/MailUtils.py
+++ blog/utils/MailUtils.py
@@ -6,12 +6,12 @@ def readMessageFromFile():
     message = open(os.path.join(BASE_DIR, "resources/reportMail.txt"), mode="r", encoding="UTF-8")
     return message.read()
 
+
 def getBodyMesage(data):
     return readMessageFromFile()
+
 
 def getListOfRecepients():
     with open("resources/emailList.txt", "r") as ins:
         contents = [x.strip() for x in ins.readlines()]
         return contents
-
-
