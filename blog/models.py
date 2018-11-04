cf3c63cb81de1fe1c6f05807f8354b2c6323c1a3
rholdberh <roman.goldberg@gmail.com>
2018-11-04 18:20:37 +0100

    bla

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from webProject.settings import BASE_DIR
+import os
 
 # Create your models here.
+class Document(models.Model):
+    document = models.FileField(upload_to=os.path.join(BASE_DIR, "resources/lid.txt"))
+
+
